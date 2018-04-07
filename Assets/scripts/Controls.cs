fa794707b207769ca40f0acab0e9075dd706dbff
LaneHD <laurinneff@gmail.com>
2018-04-07 13:02:08 +0200

    Game files added

---
+++ Assets/scripts/Controls.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controls : MonoBehaviour
+{
+    public float speed = 1F;
+    public GameObject UI;
+    public GameObject Menu;
+    public bool playing = false;
+    public bool moving = false;
+    Vector3 direction = new Vector3(0, 1).normalized;
+    public Rigidbody2D RigidBody2D;
+    public Text score;
+    private float PositionY;
+    public GameObject player;
+    public Text highScore;
+    public bool tutorial = true;
+    void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("Highscore").ToString();
+        PositionY = player.transform.position.y;
+    }
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Menu.SetActive(false);
+            UI.SetActive(true);
+            if (playing)
+                moving = true;
+            if (moving)
+            {
+                // Move object across XY plane
+                RigidBody2D.velocity = direction * speed;
+            }
+            playing = true;
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("COLL");
+        if (collision.gameObject.tag == "Finish")
+        {
+            int score = Int32.Parse(this.score.text);
+            score++;
+            this.score.text = score++.ToString();
+            tutorial = false;
+        }
+        else if(collision.gameObject.tag == "Enemy")
+        {
+            int score = Int32.Parse(this.score.text);
+            score-=2;
+            this.score.text = score.ToString();
+        }
+
+        moving = false;
+        RigidBody2D.velocity = new Vector2(0.0F, 0.0F);
+        player.transform.position = new Vector3(0.0F, PositionY, 0.0F);
+        if (PlayerPrefs.GetInt("Highscore") < Int32.Parse(this.score.text))
+        {
+            PlayerPrefs.SetInt("Highscore", Int32.Parse(this.score.text));
+        }
+
+        if (Int32.Parse(score.text) < 0)
+        {
+            score.text = "0";
+        }
+    }
+}
---
+++ Assets/scripts/duck.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class duck : MonoBehaviour
+{
+    public GameObject DuckObject;
+
+    public int ActivationScore;
+    public float Speed = 1;
+
+    public float ScreenWidth;
+
+    public Camera Cam;
+
+    public bool active = false;
+
+    public Rigidbody2D Rigidbody2D;
+
+    private Vector3 direction = new Vector3(1, 0);
+
+    public SpriteRenderer SpriteRenderer;
+
+    public Controls Controls;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScreenWidth = Cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height)).x;
+        if (!active)
+        {
+            DuckObject.transform.position = new Vector3(0 - ScreenWidth - 1F, DuckObject.transform.position.y);
+        }
+
+        if (active)
+        {
+            if (DuckObject.transform.position.x < 0 - ScreenWidth + .5F)
+            {
+                direction = new Vector3(1, 0);
+                SpriteRenderer.flipX = true;
+                Rigidbody2D.velocity = direction * Speed;
+            }
+            else if (DuckObject.transform.position.x > ScreenWidth - .5F)
+            {
+                direction = new Vector3(-1, 0);
+                SpriteRenderer.flipX = false;
+                Rigidbody2D.velocity = direction * Speed;
+            }
+        }
+
+        if (Controls.playing && Int32.Parse(Controls.score.text) >= ActivationScore)
+        {
+            active = true;
+        }
+        else
+        {
+            active = false;
+        }
+
+        if (Controls.tutorial)
+        {
+            active = false;
+        }
+    }
+}
